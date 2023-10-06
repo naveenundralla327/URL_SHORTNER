0a59602579a1ef150f0505a9ee811804d8d9dccd
undralla naveen <121333046+naveenundralla327@users.noreply.github.com>
2023-10-06 20:08:35 +0530

    Add files via upload

---
+++ copy.js
@@ -0,0 +1,9 @@
+let newURL = document.getElementById("shorturl");
+
+let coptButton = document.getElementById("copy");
+
+coptButton.onclick = ()=>{
+    newURL.select();
+    window.navigator.clipboard.writeText(newURL.value);
+}
+
